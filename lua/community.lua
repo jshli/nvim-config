fef4bfca0a41e14c627883ddcb6ccc078f13a38e
Josh Li <joshua.li@cultureamp.com>
2024-04-02 20:18:12 +1100

    init-commit

---
+++ lua/community.lua
@@ -0,0 +1,37 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.scrolling.neoscroll-nvim" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.pack.ruby" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.test.neotest" },
+
+  {
+    "nvim-neotest/neotest",
+    config = function()
+      require("neotest").setup {
+        adapters = {
+          require "neotest-rspec",
+        },
+      }
+    end,
+    dependencies = {
+      "olimorris/neotest-rspec",
+    },
+  },
+  -- import/override with your plugins folder
+}
