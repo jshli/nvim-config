dd8b104229328f0c68e14b32c3509894290afb27
Josh Li <joshua.li@cultureamp.com>
2024-04-03 10:56:09 +1100

    fix: add config to show hidden files in neotree

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -80,4 +80,14 @@ return {
       )
     end,
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        },
+      },
+    },
+  },
 }
